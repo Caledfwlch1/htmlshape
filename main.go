37545897fbc6e4491a848de5ca94ffa5d1db9176
caledfwlch1 <caledfwlch1@gmail.com>
2017-03-07 11:20:02 +0200

    read html

--- main.go
+++ main.go
@@ -8,9 +8,11 @@ import (
 	"log"
 	"io"
 	"fmt"
+	"bufio"
 )
 
 var f_out = flag.String("o","out.gv.txt","output graphviz file")
+var f_in = flag.String("i","input.html","input html file")
 var escaper = strings.NewReplacer(`"`,`\"`)
 
 func str(s string) string {
@@ -33,9 +35,9 @@ func link(w io.Writer, n1, n2 *html.Node, label string) {
 		return
 	}
 	fmt.Fprintf(w, `%s -> %s`, nodeStr(n1), nodeStr(n2))
-	if label != "" {
-		fmt.Fprintf(w, ` [ label = %s ]`, str(label))
-	}
+	//if label != "" {
+	//	fmt.Fprintf(w, ` [ label = %s ]`, str(label))
+	//}
 	fmt.Fprint(w, `;`)
 }
 
@@ -44,6 +46,9 @@ func walk(w io.Writer, n *html.Node) {
 		return
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.TextNode && strings.TrimSpace(c.Data) == "" {
+			continue
+		}
 		link(w, n, c, "child")
 		walk(w, c)
 	}
@@ -51,7 +56,7 @@ func walk(w io.Writer, n *html.Node) {
 
 func main() {
 	flag.Parse()
-	const inp = `<html>
+	inp := `<html>
 	<head>
 	<title>
 		Title of the document
@@ -65,6 +70,11 @@ func main() {
 	</div>
 	</body>
 	</html>`
+	inp, err := readFile(*f_in)
+	if err != io.EOF {
+		log.Fatal("Error reading file ", *f_in, " - ", err)
+		return
+	}
 	n, err := html.Parse(strings.NewReader(inp))
 	if err != nil {
 		log.Fatal(err)
@@ -78,4 +88,24 @@ func main() {
 	fmt.Fprint(file,"digraph html {\n")
 	walk(file, n)
 	fmt.Fprint(file,"}\n")
+}
+
+func readFile(in string) (sl string, err error) {
+
+	_, err = os.Stat(in)
+	if err != nil {
+		return "", err
+	}
+	fi, err := os.Open(in)
+	if err != nil {
+		return "", err
+	}
+	fiReader := bufio.NewReader(fi)
+	var s []byte
+	for err == nil {
+		s, _, err = fiReader.ReadLine()
+		sl += string(s)
+	}
+
+	return sl, err
 }
